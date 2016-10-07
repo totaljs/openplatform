fa218730cb5f8964a125bbc11c4d5552353261e1
Peter Širka <petersirka@gmail.com>
2016-10-07 19:11:34 +0200

    Fix Sirka's mistake.

--- models/schema-login.js
+++ models/schema-login.js
@@ -36,7 +36,7 @@ NEWSCHEMA('Login').make(function(schema) {
 		}
 
 		// Creates cookie
-		controller.cookie(CONFIG('cookie'), F.encrypt({ id: user.id, resetcounter: user.resetcounter, expire: F.datetime.add(CONFIG('cookie-expiration')) }).getTime(), '5 days');
+		controller.cookie(CONFIG('cookie'), F.encrypt({ id: user.id, resetcounter: user.resetcounter, expire: F.datetime.add(CONFIG('cookie-expiration')).getTime() }), '5 days');
 
 		// Sends a response
 		callback(SUCCESS(true));
