744ccdf17bd1432615c5e4464274ce7731c249ee
Peter Širka <petersirka@gmail.com>
2018-10-22 12:28:21 +0200

    Improved SnackBar.

--- public/js/ui.js
+++ public/js/ui.js
@@ -3345,17 +3345,17 @@ COMPONENT('app-managment', function(self, config) {
 	};
 });
 
-COMPONENT('snackbar', 'timeout:3000;button:Dismiss', function(self, config) {
+COMPONENT('snackbar', 'timeout:4000;button:OK', function(self, config) {
 
 	var show = true;
 	var callback;
-	var ti;
 
 	self.readonly();
 	self.blind();
+
 	self.make = function() {
 		self.aclass('ui-snackbar hidden');
-		self.append('<div><a href="javasc' + 'ript:void(0)" class="ui-snackbar-dismiss"></a><div class="ui-snackbar-body"></div></div>');
+		self.append('<div><span class="ui-snackbar-dismiss"></span><span class="ui-snackbar-icon"></span><div class="ui-snackbar-body"></div></div>');
 		self.event('click', '.ui-snackbar-dismiss', function() {
 			self.hide();
 			callback && callback();
@@ -3363,23 +3363,27 @@ COMPONENT('snackbar', 'timeout:3000;button:Dismiss', function(self, config) {
 	};
 
 	self.hide = function() {
+		clearTimeout2(self.ID);
 		self.rclass('ui-snackbar-visible');
-		clearTimeout(ti);
-		ti = setTimeout(function() {
+		setTimeout(function() {
 			self.aclass('hidden');
 		}, 1000);
 		show = true;
 	};
 
+	self.waiting = function(message, button, close) {
+		self.show(message, button, close, 'fa-spinner fa-pulse');
+	};
+
 	self.success = function(message, button, close) {
-		self.show('<i class="fa fa-check-circle ui-snackbar-icon"></i>' + message, button, close);
+		self.show(message, button, close, 'fa-check-circle');
 	};
 
 	self.warning = function(message, button, close) {
-		self.show('<i class="fa fa-times-circle ui-snackbar-icon"></i>' + message, button, close);
+		self.show(message, button, close, 'fa-times-circle');
 	};
 
-	self.show = function(message, button, close) {
+	self.show = function(message, button, close, icon) {
 
 		if (typeof(button) === 'function') {
 			close = button;
@@ -3388,17 +3392,18 @@ COMPONENT('snackbar', 'timeout:3000;button:Dismiss', function(self, config) {
 
 		callback = close;
 
-		self.find('.ui-snackbar-body').html(message);
+		self.find('.ui-snackbar-icon').html('<i class="fa {0}"></i>'.format(icon || 'fa-info-circle'));
+		self.find('.ui-snackbar-body').html(message).attr('title', message);
 		self.find('.ui-snackbar-dismiss').html(button || config.button);
 
 		if (show) {
 			self.rclass('hidden');
-			clearTimeout(ti);
-			ti = setTimeout(function() {
+			setTimeout(function() {
 				self.aclass('ui-snackbar-visible');
 			}, 50);
 		}
 
+		clearTimeout2(self.ID);
 		setTimeout2(self.ID, self.hide, config.timeout + 50);
 		show = false;
 	};
