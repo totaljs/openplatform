560c1ee88f2d0f7b0ce7e62af7ea073d29282dca
Peter Širka <petersirka@gmail.com>
2018-12-05 21:29:39 +0100

    Fixed preview.

--- public/js/ui.js
+++ public/js/ui.js
@@ -1193,7 +1193,7 @@ COMPONENT('preview', 'width:200;height:100;background:#FFFFFF;quality:90;schema:
 	var empty, img, canvas, name, content = null;
 
 	self.readonly();
-	self.nocompile();
+	self.nocompile && self.nocompile();
 
 	self.configure = function(key, value, init) {
 
@@ -1282,17 +1282,8 @@ COMPONENT('preview', 'width:200;height:100;background:#FFFFFF;quality:90;schema:
 		self.redraw();
 
 		self.event('change', 'input', function() {
-			var reader = new FileReader();
-			reader.onload = function () {
-				var image = new Image();
-				image.onload = function() {
-					self.resize(image);
-				};
-				image.src = reader.result;
-			};
 			var file = this.files[0];
-			name = file.name;
-			reader.readAsDataURL(file);
+			file && self.load(file);
 			this.value = '';
 		});
 
@@ -1315,18 +1306,34 @@ COMPONENT('preview', 'width:200;height:100;background:#FFFFFF;quality:90;schema:
 
 			var dt = e.originalEvent.dataTransfer;
 			if (dt && dt.files.length) {
-				var reader = new FileReader();
-				reader.onload = function () {
-					var image = new Image();
-					image.onload = function() {
-						self.resize(image);
-					};
-					image.src = reader.result;
-				};
 				var file = e.originalEvent.dataTransfer.files[0];
-				name = file.name;
-				reader.readAsDataURL(file);
+				file && self.load(file);
 			}
+		});
+	};
+
+	self.load = function(file) {
+		name = file.name;
+		self.getOrientation(file, function(orient) {
+			var reader = new FileReader();
+			reader.onload = function () {
+				var img = new Image();
+				img.onload = function() {
+					self.resize(img);
+					self.change(true);
+				};
+				img.crossOrigin = 'anonymous';
+				if (orient < 2) {
+					img.src = reader.result;
+				} else {
+					SETTER('loading', 'show');
+					self.resetOrientation(reader.result, orient, function(url) {
+						SETTER('loading', 'hide', 500);
+						img.src = url;
+					});
+				}
+			};
+			reader.readAsDataURL(file);
 		});
 	};
 
@@ -1348,6 +1355,69 @@ COMPONENT('preview', 'width:200;height:100;background:#FFFFFF;quality:90;schema:
 
 	self.setter = function(value) {
 		img.attr('src', value ? config.format.format(value) : empty);
+	};
+
+	// http://stackoverflow.com/a/32490603
+	self.getOrientation = function(file, callback) {
+		var reader = new FileReader();
+		reader.onload = function(e) {
+			var view = new DataView(e.target.result);
+			if (view.getUint16(0, false) != 0xFFD8)
+				return callback(-2);
+			var length = view.byteLength;
+			var offset = 2;
+			while (offset < length) {
+				var marker = view.getUint16(offset, false);
+				offset += 2;
+				if (marker == 0xFFE1) {
+					if (view.getUint32(offset += 2, false) != 0x45786966)
+						return callback(-1);
+					var little = view.getUint16(offset += 6, false) == 0x4949;
+					offset += view.getUint32(offset + 4, little);
+					var tags = view.getUint16(offset, little);
+					offset += 2;
+					for (var i = 0; i < tags; i++)
+						if (view.getUint16(offset + (i * 12), little) == 0x0112)
+							return callback(view.getUint16(offset + (i * 12) + 8, little));
+				} else if ((marker & 0xFF00) != 0xFF00)
+					break;
+				else
+					offset += view.getUint16(offset, false);
+			}
+			return callback(-1);
+		};
+		reader.readAsArrayBuffer(file.slice(0, 64 * 1024));
+	};
+
+	self.resetOrientation = function(src, srcOrientation, callback) {
+		var img = new Image();
+		img.onload = function() {
+			var width = img.width;
+			var height = img.height;
+			var canvas = document.createElement('canvas');
+			var ctx = canvas.getContext('2d');
+
+			// set proper canvas dimensions before transform & export
+			if (4 < srcOrientation && srcOrientation < 9) {
+				canvas.width = height;
+				canvas.height = width;
+			} else {
+				canvas.width = width;
+				canvas.height = height;
+			}
+			switch (srcOrientation) {
+				case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
+				case 3: ctx.transform(-1, 0, 0, -1, width, height); break;
+				case 4: ctx.transform(1, 0, 0, -1, 0, height); break;
+				case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
+				case 6: ctx.transform(0, 1, -1, 0, height, 0); break;
+				case 7: ctx.transform(0, -1, -1, 0, height, width); break;
+				case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
+			}
+			ctx.drawImage(img, 0, 0);
+			callback(canvas.toDataURL());
+		};
+		img.src = src;
 	};
 });
 
