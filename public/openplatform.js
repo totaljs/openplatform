634c325305ae77f6c0f5b2172154e7962f60702e
Peter Širka <petersirka@gmail.com>
2018-12-19 08:15:29 +0100

    Updated `openplatform.js`.

--- public/openplatform.js
+++ public/openplatform.js
@@ -355,7 +355,8 @@ OP.$process = function(data) {
 		var d = data.body;
 		var b = document.body.classList;
 
-		b.add(d.darkmode ? 'opdark ui-dark' : 'oplight');
+		b.add(d.darkmode ? 'opdark' : 'oplight');
+		d.darkmode && b.add('ui-dark');
 		b.add('opbody');
 		b.remove(d.darkmode ? 'oplight' : 'opdark');
 		!d.darkmode && b.remove('ui-dark');
