0b3052f5505f7e083706a0ee2b473ecaa9ba859d
Peter Širka <petersirka@gmail.com>
2018-12-29 20:56:41 +0100

    Improved tokenization.

--- schemas/login.js
+++ schemas/login.js
@@ -20,23 +20,31 @@ NEWSCHEMA('Login', function(schema) {
 					return;
 				}
 
-				// Updates token
-				user.verifytoken = U.GUID(15);
+				FUNC.sessions.get(user.id, function(err, user) {
 
-				FUNC.users.set(user, ['verifytoken'], function(err) {
+					var done = function() {
+						var cookie = {};
+						cookie.id = user.id;
+						cookie.date = NOW;
+						cookie.ua = ($.controller.req.headers['user-agent'] || '').substring(0, 20);
+						$.controller.cookie(CONF.cookie, F.encrypt(cookie), CONF.cookie_expiration || '1 month', COOKIEOPTIONS);
+						$.success();
+					};
 
-					if (err) {
-						$.invalid(err);
-						return;
+					if (user) {
+						done();
+					} else {
+						user.verifytoken = U.GUID(15);
+						FUNC.users.set(user, ['verifytoken'], function(err) {
+							if (err)
+								$.invalid(err);
+							else
+								done();
+						});
 					}
 
-					var cookie = {};
-					cookie.id = user.id;
-					cookie.date = F.datetime;
-					cookie.ua = ($.controller.req.headers['user-agent'] || '').substring(0, 20);
-					$.controller.cookie(CONF.cookie, F.encrypt(cookie), CONF.cookie_expiration || '1 month', COOKIEOPTIONS);
-					$.success();
 				});
+
 			} else
 				$.invalid('error-credentials');
 		});
