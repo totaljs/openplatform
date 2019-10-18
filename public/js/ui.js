dc5bcd0727781bea527e6f8d9f4b505893a388c3
Peter Širka <petersirka@gmail.com>
2019-10-18 13:36:17 +0200

    Improved UI.

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,15 +1,16 @@
 screen.lockOrientation && screen.lockOrientation('portrait');
 
-var TTIC = ['#1abc9c','#2ecc71','#3498db','#9b59b6','#34495e','#16a085','#2980b9','#8e44ad','#2c3e50','#f1c40f','#e67e22','#e74c3c','#d35400','#c0392b'];
+// var TTIC = ['#1abc9c','#2ecc71','#3498db','#9b59b6','#34495e','#16a085','#2980b9','#8e44ad','#2c3e50','#f1c40f','#e67e22','#e74c3c','#d35400','#c0392b'];
+
 
 Thelpers.initials = function(value) {
 	var index = value.indexOf('.');
 	var arr = value.substring(index + 1).replace(/\s{2,}/g, ' ').trim().split(' ');
 	var initials = ((arr[0].substring(0, 1) + (arr[1] || '').substring(0, 1))).toUpperCase();
-	var sum = 0;
-	for (var i = 0; i < value.length; i++)
-		sum += value.charCodeAt(i);
-	return '<span class="initials" style="background-color:{1}" title="{2}">{0}</span>'.format(initials, TTIC[sum % TTIC.length], value);
+	//var sum = 0;
+	//for (var i = 0; i < value.length; i++)
+	//	sum += value.charCodeAt(i);
+	return '<span class="initials" style="background-color:{1}" title="{2}">{0}</span>'.format(initials, user.colorscheme || '#4285f4', value);
 };
 
 Thelpers.initialsbase64 = function(value, width, height) {
