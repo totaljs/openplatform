d424e9121c447c09534e2bcf6a94acf4222ca5d1
Peter Širka <petersirka@gmail.com>
2019-11-08 08:38:01 +0100

    Fixed static scripts.

--- definitions/merge.js
+++ definitions/merge.js
@@ -2,12 +2,10 @@
 MERGE('/css/tabbed.css', '/css/ui.css', '/css/tabbed.css');
 MERGE('/css/windowed.css', '/css/ui.css', '/css/windowed.css');
 MERGE('/css/portal.css', '/css/ui.css', '/css/portal.css');
-MERGE('/css/app.css', '/css/ui.css', '/css/app.css', '/css/database.css');
-MERGE('/css/workshop.css', '/css/ui.css', '/css/workshop-data.css');
+MERGE('/css/app.css', '/css/ui.css', '/css/app.css');
 
 // Scripts
 MERGE('/js/windowed.js', '/js/ui.js', '/js/windowed.js');
 MERGE('/js/portal.js', '/js/ui.js', '/js/portal.js');
 MERGE('/js/tabbed.js', '/js/ui.js', '/js/tabbed.js');
-MERGE('/js/app.js', '/js/ui.js', '/js/app.js', '/js/database.js', '/openplatform.js');
-MERGE('/js/workshop.js', '/js/ui.js', '/js/workshop-data.js', '/openplatform.js');
+MERGE('/js/app.js', '/js/ui.js', '/js/app.js', '/openplatform.js');
