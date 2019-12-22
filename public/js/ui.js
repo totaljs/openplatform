03b6f4f969c4ee4a54245ec2bba47dc3582386ff
Peter Sirka <petersirka@gmail.com>
2019-12-22 11:00:21 +0100

    Improved UI components.

--- public/js/ui.js
+++ public/js/ui.js
@@ -1147,39 +1147,61 @@ COMPONENT('form', 'zindex:30', function(self, config) {
 
 COMPONENT('confirm', function(self) {
 
-	var is, visible = false;
+	var cls = 'ui-' + self.name;
+	var cls2 = '.' + cls;
+	var is;
+	var events = {};
 
 	self.readonly();
 	self.singleton();
-	self.nocompile();
+	self.nocompile && self.nocompile();
 
 	self.make = function() {
 
-		self.aclass('ui-confirm hidden');
+		self.aclass(cls + ' hidden');
 
 		self.event('click', 'button', function() {
-			self.hide($(this).attrd('index').parseInt());
+			self.hide(+$(this).attrd('index'));
+		});
+
+		self.event('click', cls2 + '-close', function() {
+			self.callback = null;
+			self.hide(-1);
 		});
 
 		self.event('click', function(e) {
 			var t = e.target.tagName;
 			if (t !== 'DIV')
 				return;
-			var el = self.find('.ui-confirm-body');
-			el.aclass('ui-confirm-click');
+			var el = self.find(cls2 + '-body');
+			el.aclass(cls + '-click');
 			setTimeout(function() {
-				el.rclass('ui-confirm-click');
+				el.rclass(cls + '-click');
 			}, 300);
 		});
+	};
 
-		$(window).on('keydown', function(e) {
-			if (!visible)
-				return;
-			var index = e.which === 13 ? 0 : e.which === 27 ? 1 : null;
-			if (index != null) {
-				self.find('button[data-index="{0}"]'.format(index)).trigger('click');
-				e.preventDefault();
-			}
+	events.keydown = function(e) {
+		var index = e.which === 13 ? 0 : e.which === 27 ? 1 : null;
+		if (index != null) {
+			self.find('button[data-index="{0}"]'.format(index)).trigger('click');
+			e.preventDefault();
+			e.stopPropagation();
+			events.unbind();
+		}
+	};
+
+	events.bind = function() {
+		$(W).on('keydown', events.keydown);
+	};
+
+	events.unbind = function() {
+		$(W).off('keydown', events.keydown);
+	};
+
+	self.show2 = function(message, buttons, fn) {
+		self.show(message, buttons, function(index) {
+			!index && fn();
 		});
 	};
 
@@ -1196,30 +1218,35 @@ COMPONENT('confirm', function(self) {
 				icon = '<i class="fa fa-{0}"></i>'.format(icon.toString().replace(/"/g, ''));
 			} else
 				icon = '';
-			builder.push('<button data-index="{1}">{2}{0}</button>'.format(item, i, icon));
+
+			var color = item.match(/#[0-9a-f]+/i);
+			if (color)
+				item = item.replace(color, '').trim();
+
+			builder.push('<button data-index="{1}"{3}>{2}{0}</button>'.format(item, i, icon, color ? ' style="background:{0}"'.format(color) : ''));
 		}
 
-		self.content('ui-confirm-warning', '<div class="ui-confirm-message">{0}</div>{1}'.format(message.replace(/\n/g, '<br />'), builder.join('')));
+		self.content('<div class="{0}-message">{1}</div>{2}'.format(cls, message.replace(/\n/g, '<br />'), builder.join('')));
 	};
 
 	self.hide = function(index) {
 		self.callback && self.callback(index);
-		self.rclass('ui-confirm-visible');
-		visible = false;
+		self.rclass(cls + '-visible');
+		events.unbind();
 		setTimeout2(self.id, function() {
-			$('html').rclass('noscrollconfirm');
+			$('html').rclass(cls + '-noscroll');
 			self.aclass('hidden');
 		}, 1000);
 	};
 
-	self.content = function(cls, text) {
-		$('html').aclass('noscrollconfirm');
-		!is && self.html('<div><div class="ui-confirm-body"></div></div>');
-		self.find('.ui-confirm-body').empty().append(text);
+	self.content = function(text) {
+		$('html').aclass(cls + '-noscroll');
+		!is && self.html('<div><div class="{0}-body"><span class="{0}-close"><i class="fa fa-times"></i></span></div></div>'.format(cls));
+		self.find(cls2 + '-body').append(text);
 		self.rclass('hidden');
-		visible = true;
+		events.bind();
 		setTimeout2(self.id, function() {
-			self.aclass('ui-confirm-visible');
+			self.aclass(cls + '-visible');
 		}, 5);
 	};
 });
@@ -4385,403 +4412,6 @@ COMPONENT('range', function(self, config) {
 		content = self.html();
 		self.aclass('ui-range');
 		self.redraw();
-	};
-});
-
-COMPONENT('calendar', 'today:Set today;firstday:0;close:Close;yearselect:true;monthselect:true;yearfrom:-70 years;yearto:5 years', function(self, config) {
-
-	var skip = false;
-	var skipDay = false;
-	var visible = false;
-
-	self.days = EMPTYARRAY;
-	self.months = EMPTYARRAY;
-	self.months_short = EMPTYARRAY;
-	self.years_from;
-	self.years_to;
-	self.nocompile();
-
-	self.configure = function(key, value) {
-		switch (key) {
-			case 'days':
-				if (value instanceof Array)
-					self.days = value;
-				else
-					self.days = value.split(',').trim();
-
-				for (var i = 0; i < DAYS.length; i++) {
-					DAYS[i] = self.days[i];
-					self.days[i] = DAYS[i].substring(0, 2).toUpperCase();
-				}
-
-				break;
-
-			case 'months':
-				if (value instanceof Array)
-					self.months = value;
-				else
-					self.months = value.split(',').trim();
-
-				self.months_short = [];
-
-				for (var i = 0, length = self.months.length; i < length; i++) {
-					var m = self.months[i];
-					MONTHS[i] = m;
-					if (m.length > 4)
-						m = m.substring(0, 3) + '.';
-					self.months_short.push(m);
-				}
-				break;
-
-			case 'yearfrom':
-				if (value.indexOf('current') !== -1)
-					self.years_from = parseInt(new Date().format('yyyy'));
-				else
-					self.years_from = parseInt(new Date().add(value).format('yyyy'));
-				break;
-
-			case 'yearto':
-				if (value.indexOf('current') !== -1)
-					self.years_to = parseInt(new Date().format('yyyy'));
-				else
-					self.years_to = parseInt(new Date().add(value).format('yyyy'));
-				break;
-		}
-	};
-
-	self.readonly();
-	self.click = function() {};
-
-	function getMonthDays(dt) {
-
-		var m = dt.getMonth();
-		var y = dt.getFullYear();
-
-		if (m === -1) {
-			m = 11;
-			y--;
-		}
-
-		return (32 - new Date(y, m, 32).getDate());
-	}
-
-	self.calculate = function(year, month, selected) {
-
-		var d = new Date(year, month, 1, 12, 0);
-		var output = { header: [], days: [], month: month, year: year };
-		var firstDay = config.firstday;
-		var firstCount = 0;
-		var frm = d.getDay() - firstDay;
-		var today = new Date();
-		var ty = today.getFullYear();
-		var tm = today.getMonth();
-		var td = today.getDate();
-		var sy = selected ? selected.getFullYear() : -1;
-		var sm = selected ? selected.getMonth() : -1;
-		var sd = selected ? selected.getDate() : -1;
-		var days = getMonthDays(d);
-
-		if (frm < 0)
-			frm = 7 + frm;
-
-		while (firstCount++ < 7) {
-			output.header.push({ index: firstDay, name: self.days[firstDay] });
-			firstDay++;
-			if (firstDay > 6)
-				firstDay = 0;
-		}
-
-		var index = 0;
-		var indexEmpty = 0;
-		var count = 0;
-		var prev = getMonthDays(new Date(year, month - 1, 1, 12, 0)) - frm;
-		var cur;
-
-		for (var i = 0; i < days + frm; i++) {
-
-			var obj = { isToday: false, isSelected: false, isEmpty: false, isFuture: false, number: 0, index: ++count };
-
-			if (i >= frm) {
-				obj.number = ++index;
-				obj.isSelected = sy === year && sm === month && sd === index;
-				obj.isToday = ty === year && tm === month && td === index;
-				obj.isFuture = ty < year;
-				if (!obj.isFuture && year === ty) {
-					if (tm < month)
-						obj.isFuture = true;
-					else if (tm === month)
-						obj.isFuture = td < index;
-				}
-
-			} else {
-				indexEmpty++;
-				obj.number = prev + indexEmpty;
-				obj.isEmpty = true;
-				cur = d.add('-' + indexEmpty + ' days');
-			}
-
-			if (!obj.isEmpty)
-				cur = d.add(i + ' days');
-
-			obj.month = i >= frm && obj.number <= days ? d.getMonth() : cur.getMonth();
-			obj.year = i >= frm && obj.number <= days ? d.getFullYear() : cur.getFullYear();
-			obj.date = cur;
-			output.days.push(obj);
-		}
-
-		indexEmpty = 0;
-
-		for (var i = count; i < 42; i++) {
-			var cur = d.add(i + ' days');
-			var obj = { isToday: false, isSelected: false, isEmpty: true, isFuture: true, number: ++indexEmpty, index: ++count };
-			obj.month = cur.getMonth();
-			obj.year = cur.getFullYear();
-			obj.date = cur;
-			output.days.push(obj);
-		}
-
-		return output;
-	};
-
-	self.hide = function() {
-		if (visible) {
-			self.older = null;
-			self.aclass('hidden');
-			self.rclass('ui-calendar-visible');
-			visible = false;
-		}
-		return self;
-	};
-
-	self.toggle = function(el, value, callback, offset) {
-		if (self.older === el[0]) {
-			!self.hclass('hidden') && self.hide();
-		} else {
-			self.older = el[0];
-			self.show(el, value, callback, offset);
-		}
-		return self;
-	};
-
-	self.show = function(el, value, callback, offset) {
-
-		setTimeout(function() {
-			clearTimeout2('calendarhide');
-		}, 5);
-
-		if (!el)
-			return self.hide();
-
-		var off = el.offset();
-		var h = el.innerHeight();
-		var l = off.left + (offset || 0);
-		var t = off.top + h + 12;
-		var s = 250;
-
-		if (l + s > WW) {
-			var w = el.innerWidth();
-			l = (l + w) - s;
-		}
-
-		self.css({ left: l, top: t });
-		self.rclass('hidden');
-		self.click = callback;
-		self.date(value);
-		visible = true;
-		self.aclass('ui-calendar-visible', 50);
-		return self;
-	};
-
-	self.make = function() {
-
-		self.aclass('ui-calendar hidden');
-
-		var conf = {};
-
-		if (!config.days) {
-			conf.days = [];
-			for (var i = 0; i < DAYS.length; i++)
-				conf.days.push(DAYS[i].substring(0, 2).toUpperCase());
-		}
-
-		!config.months && (conf.months = MONTHS);
-		self.reconfigure(conf);
-
-		self.event('click', '.ui-calendar-today-a', function() {
-			var dt = new Date();
-			self.hide();
-			if (self.click) {
-				if (typeof(self.click) === 'string') {
-					SET(self.click, dt);
-					CHANGE(self.click, true);
-				} else
-					self.click(dt);
-			}
-		});
-
-		self.event('click', '.ui-calendar-day', function() {
-			var arr = this.getAttribute('data-date').split('-');
-			var dt = new Date(parseInt(arr[0]), parseInt(arr[1]), parseInt(arr[2]), 12, 0);
-			self.find('.ui-calendar-selected').rclass('ui-calendar-selected');
-			var el = $(this).aclass('ui-calendar-selected');
-			skip = !el.hclass('ui-calendar-disabled');
-			self.hide();
-			if (self.click) {
-				if (typeof(self.click) === 'string') {
-					SET(self.click, dt);
-					CHANGE(self.click, true);
-				} else
-					self.click(dt);
-			}
-		});
-
-		self.event('click', '.ui-calendar-header', function(e) {
-			e.stopPropagation();
-		});
-
-		self.event('change', '.ui-calendar-year', function(e) {
-
-			clearTimeout2('calendarhide');
-			e.preventDefault();
-			e.stopPropagation();
-
-			var arr = this.getAttribute('data-date').split('-');
-			var dt = new Date(parseInt(arr[0]), parseInt(arr[1]), 1, 12, 0);
-			dt.setFullYear(this.value);
-			skipDay = true;
-			self.date(dt);
-		});
-
-		self.event('change', '.ui-calendar-month', function(e){
-
-			clearTimeout2('calendarhide');
-			e.preventDefault();
-			e.stopPropagation();
-
-			var arr = this.getAttribute('data-date').split('-');
-			var dt = new Date(parseInt(arr[0]), parseInt(arr[1]), 1, 12, 0);
-			dt.setMonth(this.value);
-			skipDay = true;
-			self.date(dt);
-		});
-
-		self.event('click', 'button', function(e) {
-
-			e.preventDefault();
-			e.stopPropagation();
-
-			var arr = this.getAttribute('data-date').split('-');
-			var dt = new Date(parseInt(arr[0]), parseInt(arr[1]), 1, 12, 0);
-			switch (this.name) {
-				case 'prev':
-					dt.setMonth(dt.getMonth() - 1);
-					break;
-				case 'next':
-					dt.setMonth(dt.getMonth() + 1);
-					break;
-			}
-
-			var current_year = dt.getFullYear();
-			if (current_year < self.years_from || current_year > self.years_to)
-				return;
-
-			skipDay = true;
-			self.date(dt);
-		});
-
-		$(window).on('scroll click', function(e) {
-			visible && setTimeout2('calendarhide', function() {
-				EXEC('$calendar.hide');
-			}, 20);
-		});
-
-		ON('scroll', function() {
-			visible && setTimeout2('calendarhide', function() {
-				EXEC('$calendar.hide');
-			}, 20);
-		});
-
-		window.$calendar = self;
-
-		self.on('reflow', function() {
-			visible && EXEC('$calendar.hide');
-		});
-	};
-
-	self.date = function(value) {
-
-		var clssel = 'ui-calendar-selected';
-
-		if (typeof(value) === 'string')
-			value = value.parseDate();
-
-		if (!value || isNaN(value.getTime())) {
-			self.find('.' + clssel).rclass(clssel);
-			value = NOW;
-		}
-
-		var empty = !value;
-
-		if (skipDay) {
-			skipDay = false;
-			empty = true;
-		}
-
-		if (skip) {
-			skip = false;
-			return;
-		}
-
-		if (!value)
-			value = NOW = new Date();
-
-		var output = self.calculate(value.getFullYear(), value.getMonth(), value);
-		var builder = [];
-
-		for (var i = 0; i < 42; i++) {
-
-			var item = output.days[i];
-
-			if (i % 7 === 0) {
-				builder.length && builder.push('</tr>');
-				builder.push('<tr>');
-			}
-
-			var cls = [];
-
-			item.isEmpty && cls.push('ui-calendar-disabled');
-			cls.push('ui-calendar-day');
-
-			!empty && item.isSelected && cls.push(clssel);
-			item.isToday && cls.push('ui-calendar-day-today');
-			builder.push('<td class="{0}" data-date="{1}-{2}-{3}"><div>{3}</div></td>'.format(cls.join(' '), item.year, item.month, item.number));
-		}
-
-		builder.push('</tr>');
-
-		var header = [];
-		for (var i = 0; i < 7; i++)
-			header.push('<th>{0}</th>'.format(output.header[i].name));
-
-		var years = value.getFullYear();
-		if (config.yearselect) {
-			years = '';
-			var current_year = value.getFullYear();
-			for (var i = self.years_from; i <= self.years_to; i++)
-				years += '<option value="{0}" {1}>{0}</option>'.format(i, i === current_year ? 'selected' : '');
-			years = '<select data-date="{0}-{1}" class="ui-calendar-year">{2}</select>'.format(output.year, output.month, years);
-		}
-
-		var months = self.months[value.getMonth()];
-		if (config.monthselect) {
-			months = '';
-			var current_month = value.getMonth();
-			for (var i = 0, l = self.months.length; i < l; i++)
-				months += '<option value="{0}" {2}>{1}</option>'.format(i, self.months[i], i === current_month ? 'selected' : '');
-			months = '<select data-date="{0}-{1}" class="ui-calendar-month">{2}</select>'.format(output.year, output.month, months);
-		}
-
-		self.html('<div class="ui-calendar-header"><button class="ui-calendar-header-prev" name="prev" data-date="{0}-{1}"><span class="fa fa-arrow-left"></span></button><div class="ui-calendar-header-info">{2} {3}</div><button class="ui-calendar-header-next" name="next" data-date="{0}-{1}"><span class="fa fa-arrow-right"></span></button></div><div class="ui-calendar-table"><table cellpadding="0" cellspacing="0" border="0"><thead>{4}</thead><tbody>{5}</tbody></table></div>'.format(output.year, output.month, months, years, header.join(''), builder.join('')) + (config.today ? '<div class="ui-calendar-today"><a href="javascript:void(0)">{0}</a><a href="javascript:void(0)" class="ui-calendar-today-a"><i class="fa fa-calendar"></i>{1}</a></div>'.format(config.close, config.today) : ''));
 	};
 });
 
