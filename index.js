66cef9cb0f12c121ffc64d94a6064a8eb2f39a6e
Peter Širka <petersirka@gmail.com>
2021-01-14 15:35:54 +0100

    Updated start script.

--- index.js
+++ index.js
@@ -3,24 +3,28 @@
 // https://www.totaljs.com
 // ===================================================
 
-const total = 'total4';
 const options = {};
 
 // options.ip = '127.0.0.1';
 // options.port = parseInt(process.argv[2]);
 // options.unixsocket = require('path').join(require('os').tmpdir(), 'app_name');
+// options.unixsocket777 = true;
 // options.config = { name: 'Total.js' };
 // options.sleep = 3000;
 // options.inspector = 9229;
 // options.watch = ['private'];
 // options.livereload = 'https://yourhostname';
 
-var type = process.argv.indexOf('--release', 1) !== -1 || process.argv.indexOf('release', 1) !== -1 ? 'release' : 'debug';
+// Enables cluster:
+// options.cluster = 'auto';
+// options.cluster_limit = 10; // max 10. threads (works only with "auto" scaling)
 
-if (total === 'total.js') {
-	if (type === 'release')
-		require(total).http('release', options);
-	else
-		require(total + '/debug')(options);
-} else
-	require(total + '/' + type)(options);
+// Enables threads:
+// options.cluster = 'auto';
+// options.cluster_limit = 10; // max 10. threads (works only with "auto" scaling)
+// options.timeout = 5000;
+// options.threads = '/api/';
+// options.logs = 'isolated';
+
+var type = process.argv.indexOf('--release', 1) !== -1 || process.argv.indexOf('release', 1) !== -1 ? 'release' : 'debug';
+require('total4/' + type)(options);
