cddb4532af0129064cdb646e93a5bce2ea05d5ec
Peter Širka <petersirka@gmail.com>
2021-03-23 15:37:15 +0100

    Improved dashboard.

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,3 +1,5 @@
+var TTIC = ['#1abc9c','#2ecc71','#3498db','#9b59b6','#34495e','#16a085','#2980b9','#8e44ad','#2c3e50','#f1c40f','#e67e22','#e74c3c','#d35400','#c0392b'];
+
 screen.lockOrientation && screen.lockOrientation('portrait');
 
 FUNC.focus = function() {
@@ -5,11 +7,17 @@ FUNC.focus = function() {
 	document.body.focus();
 };
 
-Thelpers.initials = function(value) {
+Thelpers.initials = function(value, counted) {
 	var index = value.indexOf('.');
 	var arr = value.substring(index + 1).replace(/\s{2,}/g, ' ').trim().split(' ');
 	var initials = ((arr[0].substring(0, 1) + (arr[1] || '').substring(0, 1))).toUpperCase();
-	return '<span class="initials" style="background-color:{1}" title="{2}">{0}</span>'.format(initials, user.colorscheme || '#4285f4', value);
+	var sum = 0;
+	if (counted) {
+		sum = 0;
+		for (var i = 0; i < value.length; i++)
+			sum += value.charCodeAt(i);
+	}
+	return '<span class="initials" style="background-color:{1}" title="{2}">{0}</span>'.format(initials, counted ? TTIC[sum % TTIC.length] : (user.colorscheme || '#4285f4'), value);
 };
 
 Thelpers.initialsbase64 = function(value, width, height) {
