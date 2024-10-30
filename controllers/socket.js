01d6f13d5be83e7fe1306aee376bda0c02601b6f
petersirka <petersirka@gmail.com>
2024-10-30 15:31:48 +0100

    Improved synchronization.

--- controllers/socket.js
+++ controllers/socket.js
@@ -1,22 +1,52 @@
+const RECONNECT_TIMEOUT = 10000;
+
 exports.install = function() {
-	ROUTE('SOCKET /sync/ @text', socket);
+	ROUTE('SOCKET /sync/', socket);
 };
 
 function socket($) {
 
-	$.autodestroy(() => MAIN.ws = null);
 	MAIN.ws = $;
+	$.autodestroy(() => MAIN.ws = null);
+
+	let pending = [];
+	let timeout = null;
+
+	var resend = function() {
+
+		if (timeout || !pending.length)
+			return;
+
+		let msg = pending.shift();
+		if (msg) {
+			$.send(msg.text, client => client != msg.client);
+			setImmediate(resend);
+		}
+	};
+
+	var resend_force = function() {
+		timeout = null;
+		setImmediate(resend);
+	};
 
 	$.on('open', function(client) {
-		client.authorized = CONF.sync && client.query.token == CONF.sync_token;
-		if (!client.authorized)
+		client.confirmed = CONF.sync && client.query.token == CONF.sync_token;
+		if (!client.confirmed)
 			setTimeout(client => client.close(4001), 500, client);
 	});
 
+	$.on('close', function(client) {
+		if (client.confirmed) {
+			timeout && clearTimeout(timeout);
+			timeout = setTimeout(resend_force, RECONNECT_TIMEOUT);
+		}
+	});
+
 	$.on('message', function(client, message) {
-		if (client.authorized) {
-			if (client.query.resend)
-				$.send(message);
+		if (client.confirmed) {
+			console.log(timeout, message);
+			if (timeout)
+				pending.push({ client: client, text: message });
 			else
 				$.send(message, conn => conn !== client);
 		}
